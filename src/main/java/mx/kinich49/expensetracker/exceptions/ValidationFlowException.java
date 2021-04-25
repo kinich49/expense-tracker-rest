ba5e1c0929314485d5a6803e5408f5c52ba01175
Sergio Juarez <bleeding.death@gmail.com>
2021-04-25 13:23:28 -0500

    Condition now throws ValidationFlowException

---
+++ src/main/java/mx/kinich49/expensetracker/exceptions/ValidationFlowException.java
@@ -0,0 +1,8 @@
+package mx.kinich49.expensetracker.exceptions;
+
+public class ValidationFlowException extends BusinessException {
+
+    public ValidationFlowException(String message) {
+        super(message);
+    }
+}
