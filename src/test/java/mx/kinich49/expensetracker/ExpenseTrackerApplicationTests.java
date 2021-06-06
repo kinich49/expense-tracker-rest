a10d7037871a6d5a47d45f6601aceaeb4cf6d073
Sergio Juarez <bleeding.death@gmail.com>
2021-06-06 02:00:22 -0500

    Basic integration test now uses the test profile

--- src/test/java/mx/kinich49/expensetracker/ExpenseTrackerApplicationTests.java
+++ src/test/java/mx/kinich49/expensetracker/ExpenseTrackerApplicationTests.java
@@ -2,8 +2,10 @@ package mx.kinich49.expensetracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ExpenseTrackerApplicationTests {
 
     @Test
