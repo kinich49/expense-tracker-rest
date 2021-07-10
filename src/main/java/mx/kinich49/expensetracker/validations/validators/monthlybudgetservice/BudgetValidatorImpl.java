0bf33d06a2f230a531dada479d205aba0ee32b0c
Sergio Juarez <bleeding.death@gmail.com>
2021-07-10 09:48:12 -0500

    BudgetValidator extends abstract base class

--- src/main/java/mx/kinich49/expensetracker/validations/validators/monthlybudgetservice/BudgetValidatorImpl.java
+++ src/main/java/mx/kinich49/expensetracker/validations/validators/monthlybudgetservice/BudgetValidatorImpl.java
@@ -1,45 +1,17 @@
 package mx.kinich49.expensetracker.validations.validators.monthlybudgetservice;
 
 import lombok.Data;
-import mx.kinich49.expensetracker.exceptions.BusinessException;
 import mx.kinich49.expensetracker.models.web.requests.MonthlyBudgetRequest;
-import mx.kinich49.expensetracker.utils.StringUtils;
-import mx.kinich49.expensetracker.validations.Validator;
+import mx.kinich49.expensetracker.validations.AbstractValidator;
 import mx.kinich49.expensetracker.validations.ValidatorParameter;
-import mx.kinich49.expensetracker.validations.conditions.monthlybudgetservice.BudgetLimitCondition;
-import mx.kinich49.expensetracker.validations.conditions.monthlybudgetservice.BudgetRequestCondition;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class BudgetValidatorImpl implements Validator<BudgetValidatorImpl.Parameter> {
+public class BudgetValidatorImpl
+        extends AbstractValidator<BudgetValidatorImpl.Parameter> {
 
-    private final BudgetLimitCondition budgetLimitCondition;
-    private final BudgetRequestCondition budgetRequestCondition;
-
-    @Autowired
-    public BudgetValidatorImpl(BudgetLimitCondition budgetLimitCondition,
-                               BudgetRequestCondition budgetRequestCondition) {
-        this.budgetLimitCondition = budgetLimitCondition;
-        this.budgetRequestCondition = budgetRequestCondition;
-    }
-
-    @Override
-    public void validate(Parameter param) throws BusinessException {
-        MonthlyBudgetRequest request = param.request;
-        StringBuilder accumulator = new StringBuilder();
-
-        budgetRequestCondition.assertCondition(new BudgetRequestCondition.Parameter(request))
-                .ifPresent(accumulator::append);
-
-        budgetLimitCondition.assertCondition(new BudgetLimitCondition.Parameter(request))
-                .ifPresent(accumulator::append);
-
-        String error = accumulator.toString().trim();
-
-        if (StringUtils.isNeitherNullNorEmptyNorBlank(error)) {
-            throw new BusinessException(error.trim());
-        }
+    public BudgetValidatorImpl(BudgetValidatorConditionProviderImpl conditionProvider) {
+        super(conditionProvider);
     }
 
     @Data
