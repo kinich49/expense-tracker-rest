3fb49c576c523bd6c8b14bc40a35220de34097f8
Sergio Juarez <bleeding.death@gmail.com>
2021-07-10 08:50:44 -0500

    ThreadLocal in AbstractConditionProvider now initialized with an empty queue. Adds convenient method in AbstractConditionProvider to add conditions to the queue

--- src/main/java/mx/kinich49/expensetracker/validations/AbstractConditionProvider.java
+++ src/main/java/mx/kinich49/expensetracker/validations/AbstractConditionProvider.java
@@ -2,13 +2,14 @@ package mx.kinich49.expensetracker.validations;
 
 import mx.kinich49.expensetracker.utils.Pair;
 
+import java.util.ArrayDeque;
 import java.util.Objects;
 import java.util.Queue;
 
 public abstract class AbstractConditionProvider<P extends ValidatorParameter> implements ConditionProvider<P> {
 
     protected final ThreadLocal<Queue<Pair<Condition<? extends ConditionParameter>, ? extends ConditionParameter>>>
-            threadLocal = new ThreadLocal<>();
+            threadLocal = ThreadLocal.withInitial(ArrayDeque::new);
 
     @Override
     public Pair<Condition<? extends ConditionParameter>, ? extends ConditionParameter> getNextCondition() {
@@ -19,6 +20,12 @@ public abstract class AbstractConditionProvider<P extends ValidatorParameter> im
     public boolean hasNextCondition() {
         return !Objects.isNull(threadLocal.get()) &&
                 !Objects.isNull(threadLocal.get().peek());
+    }
+
+    protected void addCondition(Condition<? extends ConditionParameter> condition,
+                                ConditionParameter conditionParameter) {
+        var queue = threadLocal.get();
+        queue.add(Pair.of(condition, conditionParameter));
     }
 
 }
