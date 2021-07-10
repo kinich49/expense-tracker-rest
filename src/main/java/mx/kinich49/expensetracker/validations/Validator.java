fd6173786ec758951c4944bd963ec4005e9ab90a
Sergio Juarez <bleeding.death@gmail.com>
2021-07-10 09:40:33 -0500

    Enhances Validator javadoc

--- src/main/java/mx/kinich49/expensetracker/validations/Validator.java
+++ src/main/java/mx/kinich49/expensetracker/validations/Validator.java
@@ -3,10 +3,17 @@ package mx.kinich49.expensetracker.validations;
 import mx.kinich49.expensetracker.exceptions.BusinessException;
 
 /**
- * An interface to declare a combination of {@link Condition}
+ * Implementations of this interface will have at least one {@link Condition}.
+ * If the implementation has more than one, then it is understood those
+ * conditions are thigh-coupled.
  * <p>
- * If at least one condition is not satisfied, then a {@link BusinessException} should be thrown.
+ * If at least one condition is not satisfied,
+ * then a {@link BusinessException} <b>must</b> be thrown.
  * The exception's message should explain the error in a user-friendly manner
+ * <p>
+ * If all conditions are met, then the parameter is declared "valid",
+ * meaning all Business Rules, requirements or demands are satisfied,
+ * and the validator client can continue its process
  */
 public interface Validator<T extends ValidatorParameter> {
 
