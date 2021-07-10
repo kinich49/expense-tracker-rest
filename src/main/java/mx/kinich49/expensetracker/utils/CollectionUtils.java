b25dd90877ea992fc0db1ca9294b0137dda4fa7b
Sergio Juarez <bleeding.death@gmail.com>
2021-07-10 08:33:38 -0500

    Adds utility method to check if a collection is null or empty

--- src/main/java/mx/kinich49/expensetracker/utils/CollectionUtils.java
+++ src/main/java/mx/kinich49/expensetracker/utils/CollectionUtils.java
@@ -1,10 +1,15 @@
 package mx.kinich49.expensetracker.utils;
 
+import java.util.Collection;
 import java.util.List;
 
 public class CollectionUtils {
 
     public static <T> boolean isNeitherNullNorEmpty(List<T> list) {
         return (list != null) && !list.isEmpty();
+    }
+
+    public static <T> boolean isNullOrEmpty(Collection<T> collection) {
+        return collection == null || collection.isEmpty();
     }
 }
